88da616e226ee58081fd1ff38aef1f6cea80da7b
cutratefuture <christian@cutratefuture.com>
2020-09-03 12:34:41 -0700

    home page sketched out

--- src/components/Lander.vue
+++ src/components/Lander.vue
@@ -9,17 +9,25 @@
     </b-row>
     <b-row>
       <b-col md="12">
-        <ul class="socialList">
+        <ul class="red text-center social">
           <li>
             <a href="https://github.com/cutratefuture" target="_blank">
               <img src="../assets/icons/gh.png" alt="github icon" />
             </a>
           </li>
+
           <li>
             <a href="https://instagram.com/cutratefuture" target="_blank">
               <img src="../assets/icons/in.png" alt="instagram icon" />
             </a>
           </li>
+
+          <li>
+            <a href="https://www.deviantart.com/cutratefuture" target="_blank">
+              <img src="../assets/icons/da.png" alt="deviant art icon" />
+            </a>
+          </li>
+
           <li>
             <a href="https://soundcloud.com/cutratefuture" target="_blank">
               <img src="../assets/icons/sc.png" alt="soundcloud icon" />
@@ -42,16 +50,33 @@ export default {
 
 <style lang="css" scoped>
 .hero img {
-  height: 12em;
+  height: 13.3em;
   background-color: #e8e9eb;
+  padding: 1.1em;
 }
+
 ul {
   list-style-type: none;
   margin: 0;
+  height: 1em;
+  width: 13em;
   padding: 0;
 }
-li {
+
+.social {
+  padding-left: 18px;
 }
+
+ul li {
+  display: inline !important;
+}
+
+li > a > img {
+  margin-top: 1em;
+  margin-right: 20px;
+  height: 23px;
+}
+
 img {
   height: 20px;
   margin-right: 5px;
